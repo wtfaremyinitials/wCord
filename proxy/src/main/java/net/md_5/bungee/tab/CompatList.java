c3383b662f556f72b31c496a8dbc500fc88a4ae1
Myles <mylescomputer@gmail.com>
2014-09-08 21:48:34 +0100

    Small tab overhaul to fix bugs (reverted from commit 4de5aef473886411a94a69011de2616ef5192b3c)

--- proxy/src/main/java/net/md_5/bungee/tab/CompatList.java
+++ proxy/src/main/java/net/md_5/bungee/tab/CompatList.java
@@ -14,7 +14,6 @@ import java.util.Collection;
 public class CompatList extends TabList {
 
 	private boolean sentPing;
-	private String name;
 
 	public CompatList(ProxiedPlayer player) {
 		super(player);
@@ -31,17 +30,34 @@ public class CompatList extends TabList {
 			sentPing = true;
 			for (ProxiedPlayer p : BungeeCord.getInstance().getPlayers()) {
 				PlayerListItem packet = new PlayerListItem();
-				packet.setAction(PlayerListItem.Action.ADD_PLAYER);
+				packet.setAction(PlayerListItem.Action.UPDATE_LATENCY);
 				PlayerListItem.Item item = new PlayerListItem.Item();
 				item.setUuid(player.getUniqueId());
 				item.setUsername(player.getName());
-				item.setDisplayName(p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(this.name)) : this.name);
+				item.setDisplayName(p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(player.getDisplayName())) :
+						player.getDisplayName());
 				item.setPing(player.getPing());
 				packet.setItems(new PlayerListItem.Item[]
 						{
 								item
 						});
-				p.unsafe().sendPacket(packet);
+				if (p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT) {
+					p.unsafe().sendPacket(packet);
+				}else
+				{
+					PlayerListItem packet2 = new PlayerListItem();
+					packet2.setAction(PlayerListItem.Action.REMOVE_PLAYER);
+					PlayerListItem.Item item2 = new PlayerListItem.Item();
+					item2.setUuid(player.getUniqueId());
+					item2.setUsername(player.getName());
+					item2.setDisplayName(player.getName());
+					item2.setPing(player.getPing());
+					packet2.setItems(new PlayerListItem.Item[]
+							{
+									item2
+							});
+					p.unsafe().sendPacket(packet2);
+				}
 				packet.setAction(PlayerListItem.Action.UPDATE_DISPLAY_NAME);
 				p.unsafe().sendPacket(packet);
 			}
@@ -55,8 +71,6 @@ public class CompatList extends TabList {
 
 	@Override
 	public void onConnect() {
-		this.sentPing = false;
-		this.name = player.getDisplayName();
 		PlayerListItem playerListItem = new PlayerListItem();
 		playerListItem.setAction(PlayerListItem.Action.ADD_PLAYER);
 		Collection<ProxiedPlayer> players = BungeeCord.getInstance().getPlayers();
@@ -67,8 +81,7 @@ public class CompatList extends TabList {
 			PlayerListItem.Item item = items[i++] = new PlayerListItem.Item();
 			item.setUuid(p.getUniqueId());
 			item.setUsername(p.getName());
-			item.setDisplayName(player.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(p.getDisplayName())) : p.getDisplayName
-					());
+			item.setDisplayName(player.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(p.getDisplayName())) : p.getDisplayName());
 			LoginResult loginResult = ((UserConnection) p).getPendingConnection().getLoginProfile();
 			if (loginResult != null) {
 				String[][] props = new String[loginResult.getProperties().length][];
@@ -89,8 +102,19 @@ public class CompatList extends TabList {
 		}
 		if (player.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT) {
 			player.unsafe().sendPacket(playerListItem);
-			playerListItem.setAction(PlayerListItem.Action.UPDATE_DISPLAY_NAME);
-			player.unsafe().sendPacket(playerListItem);
+			for (ProxiedPlayer p : players) {
+				PlayerListItem.Item item = new PlayerListItem.Item();
+				item.setUuid(p.getUniqueId());
+				item.setUsername(p.getName());
+				item.setDisplayName(ComponentSerializer.toString(TextComponent.fromLegacyText(p.getDisplayName())));
+				PlayerListItem packet = new PlayerListItem();
+				packet.setAction(PlayerListItem.Action.UPDATE_DISPLAY_NAME);
+				packet.setItems(new PlayerListItem.Item[]
+						{
+								item
+						});
+				player.unsafe().sendPacket(packet);
+			}
 		} else {
 			// Split up the packet
 			for (PlayerListItem.Item item : playerListItem.getItems()) {
@@ -104,13 +128,16 @@ public class CompatList extends TabList {
 				player.unsafe().sendPacket(packet);
 			}
 		}
+
+
 		for (ProxiedPlayer p : BungeeCord.getInstance().getPlayers()) {
 			PlayerListItem packet = new PlayerListItem();
 			packet.setAction(PlayerListItem.Action.ADD_PLAYER);
 			PlayerListItem.Item item = new PlayerListItem.Item();
 			item.setUuid(player.getUniqueId());
 			item.setUsername(player.getName());
-			item.setDisplayName(p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(this.name)) : this.name);
+			item.setDisplayName(p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(player.getDisplayName())) :
+					player.getDisplayName());
 			LoginResult loginResult = ((UserConnection) player).getPendingConnection().getLoginProfile();
 			if (loginResult != null) {
 				String[][] props = new String[loginResult.getProperties().length][];
@@ -133,25 +160,22 @@ public class CompatList extends TabList {
 							item
 					});
 			p.unsafe().sendPacket(packet);
-
 		}
 
 	}
 
 	@Override
 	public void onDisconnect() {
-		for (ProxiedPlayer p : BungeeCord.getInstance().getPlayers()) {
-			PlayerListItem packet = new PlayerListItem();
-			packet.setAction(PlayerListItem.Action.REMOVE_PLAYER);
-			PlayerListItem.Item item = new PlayerListItem.Item();
-			item.setUuid(player.getUniqueId());
-			item.setUsername(player.getName());
-			item.setDisplayName(p.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_SNAPSHOT ? ComponentSerializer.toString(TextComponent.fromLegacyText(this.name)) : this.name);
-			packet.setItems(new PlayerListItem.Item[]
-					{
-							item
-					});
-			p.unsafe().sendPacket(packet);
-		}
+		PlayerListItem packet = new PlayerListItem();
+		packet.setAction(PlayerListItem.Action.REMOVE_PLAYER);
+		PlayerListItem.Item item = new PlayerListItem.Item();
+		item.setUuid(player.getUniqueId());
+		item.setUsername(player.getName());
+		item.setDisplayName(player.getDisplayName());
+		packet.setItems(new PlayerListItem.Item[]
+				{
+						item
+				});
+		BungeeCord.getInstance().broadcast(packet);
 	}
 }
